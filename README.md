634a64a86edba286e07748bf6cdb98728148e7a6
andi-neck <andi.neck@intesso.com>
2015-10-26 07:48:22 +0100

    adds tests for all commands

--- README.md
+++ README.md
@@ -30,13 +30,15 @@ Commands:
 
 install    temporary adds the local module to the package.json, runs `npm install`, and removes them from package.json again.
 uninstall  removes the local modules from the package.json, and runs `npm prune`, to remove the unneeded but installed modules.
+link       links the local modules into the `node_modules` folder.
+unlink     removes links of the local modules inside the `node_modules` folder.
 add        adds the local modules to the package.json
 remove     removes the local modules from the package.json
 
 Options:
 --dir      local module dirname default:`local_modules`
 --tmp      temporary package.json file ending during install
---force    forced action
+--force    forced action: remove stuff, before copying or linking
 
 ```
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var lm = require('../index');
+var fs = require('fs');
 var path = require('path');
 var version = require('../package.json').version;
 
@@ -19,18 +20,118 @@ test('should print help', function(t) {
 
 test('should add and remove local modules to the package.json', function(t) {
 
-  var dependencies = null;
   lm({cmd: 'add', dir: 'local_modules'});
-
-  dependencies = require(path.resolve('./package.json')).dependencies;
-  t.equal(dependencies.awesome, 'file:local_modules/awesome');
-  t.equal(dependencies.rad, 'file:local_modules/rad');
+  var added = parsePackageJson().dependencies;
+  t.equal(added.awesome, 'file:local_modules/awesome');
+  t.equal(added.rad, 'file:local_modules/rad');
 
   lm({cmd: 'remove', dir: 'local_modules'});
-
-  dependencies = require(path.resolve('./package.json')).dependencies;
-  t.equal(dependencies.awesome, undefined);
-  t.equal(dependencies.rad, undefined);
+  var removed = parsePackageJson().dependencies;
+  t.equal(removed.awesome, undefined);
+  t.equal(removed.rad, undefined);
 
   t.end();
+
 });
+
+test('should install and uninstall local modules', function(t) {
+
+  var awesome, rad;
+
+  lm({cmd: 'install', dir: 'local_modules'});
+  var installed = parsePackageJson().dependencies;
+  t.equal(installed.awesome, undefined);
+  t.equal(installed.rad, undefined);
+  t.equal(moduleStat('awesome').isDirectory(), true);
+  t.equal(moduleStat('rad').isDirectory(), true);
+
+  lm({cmd: 'uninstall', dir: 'local_modules'});
+  var uninstalled = parsePackageJson().dependencies;
+  t.equal(uninstalled.awesome, undefined);
+  t.equal(uninstalled.rad, undefined);
+  t.equal(moduleStat('awesome'), null);
+  t.equal(moduleStat('rad'), null);
+
+  t.end();
+
+});
+
+test('should link and unlink local modules', function(t) {
+
+  lm({cmd: 'link', dir: 'local_modules'});
+  var linked = parsePackageJson().dependencies;
+  t.equal(linked.awesome, undefined);
+  t.equal(linked.rad, undefined);
+  t.equal(moduleStat('awesome').isSymbolicLink(), true);
+  t.equal(moduleStat('rad').isSymbolicLink(), true);
+
+  lm({cmd: 'unlink', dir: 'local_modules'});
+  var unlinked = parsePackageJson().dependencies;
+  t.equal(unlinked.awesome, undefined);
+  t.equal(unlinked.rad, undefined);
+  t.equal(moduleStat('awesome'), null);
+  t.equal(moduleStat('rad'), null);
+
+  t.end();
+
+});
+
+
+test('should first link, install and then unlink local modules with force', function(t) {
+
+  lm({cmd: 'link', dir: 'local_modules', force: true});
+  t.equal(moduleStat('awesome').isSymbolicLink(), true);
+  t.equal(moduleStat('rad').isSymbolicLink(), true);
+
+  lm({cmd: 'install', dir: 'local_modules', force: true});
+  t.equal(moduleStat('awesome').isDirectory(), true);
+  t.equal(moduleStat('rad').isDirectory(), true);
+
+  lm({cmd: 'unlink', dir: 'local_modules', force: true});
+  t.equal(moduleStat('awesome'), null);
+  t.equal(moduleStat('rad'), null);
+
+  t.end();
+
+});
+
+
+test('should first install, link and then unlink local modules with force', function(t) {
+
+  lm({cmd: 'install', dir: 'local_modules', force: true});
+  t.equal(moduleStat('awesome').isDirectory(), true);
+  t.equal(moduleStat('rad').isDirectory(), true);
+
+  lm({cmd: 'link', dir: 'local_modules', force: true});
+  t.equal(moduleStat('awesome').isSymbolicLink(), true);
+  t.equal(moduleStat('rad').isSymbolicLink(), true);
+
+  lm({cmd: 'unlink', dir: 'local_modules', force: true});
+  t.equal(moduleStat('awesome'), null);
+  t.equal(moduleStat('rad'), null);
+
+  t.end();
+
+});
+
+function parsePackageJson() {
+
+  var file = path.resolve('./package.json');
+  var packageJson = fs.readFileSync(file, 'utf8');
+  return JSON.parse(packageJson);
+
+}
+
+function moduleStat(module){
+
+  var p = path.resolve(path.join('node_modules', module));
+
+  var stats;
+  try {
+    fs.lstatSync(p);
+  } catch (e) {
+    stats = null;
+  }
+  return stats;
+
+}
